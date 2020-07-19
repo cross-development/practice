0572bd9c2cb65569493a9ae67a1147edddd53b71
Vitaliy Derda <derdavitaliy@gmail.com>
2020-07-19 12:16:23 +0300

    added practice: module 5 context

---
+++ webinars/react/module-05-ctx/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../views/Home';
+import Blog from '../views/Blog';
+import Profile from '../views/Profile';
+import Patterns from '../views/Patterns';
+import NotFound from '../views/NotFound';
+import Layout from './Layout';
+import routes from '../routes';
+import AuthContext from '../contexts/Auth';
+
+export default class App extends Component {
+	render() {
+		return (
+			<AuthContext>
+				<Layout>
+					<Switch>
+						<Route path={routes.home} exact component={Home} />
+						<Route path={routes.blog} exact component={Blog} />
+						<Route path={routes.profile} exact component={Profile} />
+						<Route path={routes.patterns} exact component={Patterns} />
+						<Route component={NotFound} />
+					</Switch>
+				</Layout>
+			</AuthContext>
+		);
+	}
+}
