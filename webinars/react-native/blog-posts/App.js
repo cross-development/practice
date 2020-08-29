f9002d363284b5ad6edcdc163c59db00e7513f88
Vitaliy Derda <derdavitaliy@gmail.com>
2020-08-29 13:30:35 +0300

    added font and device orientation, control element

--- webinars/react-native/blog-posts/App.js
+++ webinars/react-native/blog-posts/App.js
@@ -1,51 +1,95 @@
-import React, { useState } from 'react';
-import {
-	StyleSheet,
-	TextInput,
-	View,
-	ImageBackground,
-	Text,
-	TouchableOpacity,
-	Keyboard,
-} from 'react-native';
-import bgImage from './assets/bg-img/bg-food.jpg';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, View, ImageBackground, Text } from 'react-native';
+import { TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Dimensions } from 'react-native';
+import bgImage from './assets/images/bg-food.jpg';
+import { loadAsync } from 'expo-font';
+import { AppLoading } from 'expo';
+
+const initialState = {
+	email: '',
+	password: '',
+};
+
+const loadApplication = async () => {
+	await loadAsync({
+		ArchitectsDaughter: require('./assets/fonts/architects_daughter/ArchitectsDaughter-Regular.ttf'),
+	});
+};
 
 export default function App() {
 	const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+	const [state, setState] = useState(initialState);
+	const [isReady, setIsReady] = useState(false);
+
+	const [dimensions, setDimensions] = useState(Dimensions.get('window').width - 25 * 2);
+
+	useEffect(() => {
+		const onChange = () => {
+			const width = Dimensions.get('window').width - 25 * 2;
+			setDimensions(width);
+		};
+		Dimensions.addEventListener('change', onChange);
+
+		return () => {
+			Dimensions.removeEventListener('change', onChange);
+		};
+	}, []);
 
 	const keyboardHide = () => {
 		setIsShowKeyboard(false);
 		Keyboard.dismiss();
+		setState(initialState);
 	};
+
+	const changeInputEmail = value => setState(prevState => ({ ...prevState, email: value }));
+	const changeInputPass = value => setState(prevState => ({ ...prevState, password: value }));
+
+	if (!isReady) {
+		return (
+			<AppLoading
+				startAsync={loadApplication}
+				onFinish={() => setIsReady(true)}
+				onError={console.warn}
+			/>
+		);
+	}
+
 	return (
-		<View style={styles.container}>
-			<ImageBackground style={styles.bgImage} source={bgImage}>
-				<View style={styles.form}>
-					<View>
-						<Text style={styles.inputLabel}>EMAIL ADDRESS</Text>
-						<TextInput
-							style={styles.textInput}
-							onFocus={() => setIsShowKeyboard(true)}
-						/>
+		<TouchableWithoutFeedback onPress={keyboardHide}>
+			<View style={styles.container}>
+				<ImageBackground style={styles.bgImage} source={bgImage}>
+					<View style={{ ...styles.form, width: dimensions }}>
+						<View>
+							<Text style={styles.inputLabel}>EMAIL ADDRESS</Text>
+							<TextInput
+								style={styles.textInput}
+								onFocus={() => setIsShowKeyboard(true)}
+								value={state.email}
+								onChangeText={changeInputEmail}
+							/>
+						</View>
+						<View style={{ marginTop: 20 }}>
+							<Text style={styles.inputLabel}>PASSWORD</Text>
+							<TextInput
+								style={styles.textInput}
+								secureTextEntry={true}
+								onFocus={() => setIsShowKeyboard(true)}
+								value={state.password}
+								onChangeText={changeInputPass}
+							/>
+						</View>
+						<TouchableOpacity
+							style={styles.button}
+							activeOpacity={0.8}
+							onPress={keyboardHide}
+						>
+							<Text style={styles.buttonLabel}>SIGN IN</Text>
+						</TouchableOpacity>
 					</View>
-					<View style={{ marginTop: 20 }}>
-						<Text style={styles.inputLabel}>PASSWORD</Text>
-						<TextInput
-							style={styles.textInput}
-							secureTextEntry={true}
-							onFocus={() => setIsShowKeyboard(true)}
-						/>
-					</View>
-					<TouchableOpacity
-						style={styles.button}
-						activeOpacity={0.8}
-						onPress={keyboardHide}
-					>
-						<Text style={styles.buttonLabel}>SIGN IN</Text>
-					</TouchableOpacity>
-				</View>
-			</ImageBackground>
-		</View>
+				</ImageBackground>
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
@@ -59,17 +103,18 @@ const styles = StyleSheet.create({
 		flex: 1,
 		resizeMode: 'cover',
 		justifyContent: 'center',
+		alignItems: 'center',
 	},
 
 	form: {
 		backgroundColor: 'rgba(0, 0, 0, 0.5)',
-		marginHorizontal: 50,
+		// marginHorizontal: 50,
 		borderWidth: 1,
 		borderColor: '#fffaf0',
 		paddingTop: 30,
 		paddingBottom: 30,
-		paddingLeft: 10,
-		paddingRight: 10,
+		paddingLeft: 20,
+		paddingRight: 20,
 		borderRadius: 30,
 	},
 
@@ -78,6 +123,7 @@ const styles = StyleSheet.create({
 		color: '#fffaf0',
 		fontSize: 18,
 		marginBottom: 6,
+		fontFamily: 'ArchitectsDaughter',
 	},
 
 	textInput: {
@@ -102,5 +148,6 @@ const styles = StyleSheet.create({
 	buttonLabel: {
 		color: '#fffaf0',
 		fontSize: 18,
+		fontFamily: 'ArchitectsDaughter',
 	},
 });
