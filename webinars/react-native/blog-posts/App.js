f721fe54046db23f879b8bbd87b8e08311d056ef
Vitaliy Derda <derdavitaliy@gmail.com>
2020-08-29 10:23:13 +0300

    added hide keyboard

--- webinars/react-native/blog-posts/App.js
+++ webinars/react-native/blog-posts/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { StyleSheet, TextInput, View, ImageBackground, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	TextInput,
+	View,
+	ImageBackground,
+	Text,
+	TouchableOpacity,
+	Keyboard,
+} from 'react-native';
 import bgImage from './assets/bg-img/bg-food.jpg';
 
 export default function App() {
+	const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+	const keyboardHide = () => {
+		setIsShowKeyboard(false);
+		Keyboard.dismiss();
+	};
 	return (
 		<View style={styles.container}>
 			<ImageBackground style={styles.bgImage} source={bgImage}>
 				<View style={styles.form}>
 					<View>
 						<Text style={styles.inputLabel}>EMAIL ADDRESS</Text>
-						<TextInput style={styles.textInput} />
+						<TextInput
+							style={styles.textInput}
+							onFocus={() => setIsShowKeyboard(true)}
+						/>
 					</View>
 					<View style={{ marginTop: 20 }}>
 						<Text style={styles.inputLabel}>PASSWORD</Text>
-						<TextInput style={styles.textInput} secureTextEntry={true} />
+						<TextInput
+							style={styles.textInput}
+							secureTextEntry={true}
+							onFocus={() => setIsShowKeyboard(true)}
+						/>
 					</View>
+					<TouchableOpacity
+						style={styles.button}
+						activeOpacity={0.8}
+						onPress={keyboardHide}
+					>
+						<Text style={styles.buttonLabel}>SIGN IN</Text>
+					</TouchableOpacity>
 				</View>
 			</ImageBackground>
 		</View>
@@ -31,18 +59,18 @@ const styles = StyleSheet.create({
 		flex: 1,
 		resizeMode: 'cover',
 		justifyContent: 'center',
-		// alignItems: 'center',
 	},
 
 	form: {
+		backgroundColor: 'rgba(0, 0, 0, 0.5)',
 		marginHorizontal: 50,
 		borderWidth: 1,
 		borderColor: '#fffaf0',
-		paddingTop: 40,
-		paddingBottom: 40,
+		paddingTop: 30,
+		paddingBottom: 30,
 		paddingLeft: 10,
 		paddingRight: 10,
-		borderRadius: 20,
+		borderRadius: 30,
 	},
 
 	inputLabel: {
@@ -59,6 +87,20 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		textAlign: 'center',
 		padding: 5,
+		fontSize: 18,
+	},
+
+	button: {
+		backgroundColor: '#eb4d4b',
+		height: 40,
+		borderRadius: 10,
+		marginTop: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	buttonLabel: {
+		color: '#fffaf0',
 		fontSize: 18,
 	},
 });
