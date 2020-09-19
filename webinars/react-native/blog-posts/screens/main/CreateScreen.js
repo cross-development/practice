fdcbe53c957d52e227bf62ee65cc232db675b255
Vitaliy Derda <derdavitaliy@gmail.com>
2020-09-19 23:59:47 +0300

    added camera

--- webinars/react-native/blog-posts/screens/main/CreateScreen.js
+++ webinars/react-native/blog-posts/screens/main/CreateScreen.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const CreateScreen = () => {
+const CreateScreen = ({ navigation }) => {
+	const [hasPermission, setHasPermission] = useState(null);
 	const [camera, setCamera] = useState(null);
 	const [photo, setPhoto] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
+
+	if (hasPermission === null) {
+		return <View />;
+	}
+
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
 
 	const takePhoto = async () => {
 		const photo = await camera.takePictureAsync();
 		setPhoto(photo.uri);
+	};
+
+	sendPhoto = () => {
+		navigation.navigate('Posts', { photo });
 	};
 
 	return (
@@ -20,6 +40,7 @@ const CreateScreen = () => {
 						<Image source={{ uri: photo }} style={{ height: 200, width: 200 }} />
 					</View>
 				)}
+
 				<TouchableOpacity
 					activeOpacity={0.6}
 					style={styles.snapContainer}
@@ -28,6 +49,10 @@ const CreateScreen = () => {
 					<Text style={styles.snap}>SNAP</Text>
 				</TouchableOpacity>
 			</Camera>
+
+			<TouchableOpacity activeOpacity={0.6} style={styles.sendBtn} onPress={sendPhoto}>
+				<Text style={styles.sendLabel}>SEND</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -38,7 +63,11 @@ const styles = StyleSheet.create({
 	},
 
 	camera: {
-		flex: 1,
+		// flex: 1,
+		height: '70%',
+		marginHorizontal: 2,
+		marginTop: 30,
+		borderRadius: 10,
 		alignItems: 'center',
 		justifyContent: 'flex-end',
 	},
@@ -48,7 +77,7 @@ const styles = StyleSheet.create({
 		height: 70,
 		borderWidth: 1,
 		marginBottom: 20,
-		borderRadius: 50,
+		borderRadius: 10,
 		alignItems: 'center',
 		borderColor: '#ff0000',
 		justifyContent: 'center',
@@ -64,6 +93,22 @@ const styles = StyleSheet.create({
 		borderWidth: 1,
 		borderColor: '#fff',
 		position: 'absolute',
+	},
+
+	sendBtn: {
+		marginHorizontal: 30,
+		height: 40,
+		borderWidth: 2,
+		borderColor: '#20b2aa',
+		borderRadius: 10,
+		marginTop: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	sendLabel: {
+		color: '#20b2aa',
+		fontSize: 20,
 	},
 });
 
--- webinars/react-native/blog-posts/screens/main/PostsScreen.js
+++ webinars/react-native/blog-posts/screens/main/PostsScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const PostsScreen = () => {
+const PostsScreen = ({ route }) => {
 	return (
 		<View style={styles.container}>
 			<Text>PostsScreen</Text>
