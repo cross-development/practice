8ba338f7e892e0ec85c73d94356cf48859fe4305
Vitaliy Derda <derdavitaliy@gmail.com>
2020-12-19 23:15:10 +0200

    added file-api practice

---
+++ simple-code/node-code/file-api/client/src/components/GeoCadsItem/GeoCadsItem.js
@@ -0,0 +1,31 @@
+//Core
+import React from 'react';
+
+const GeoCadsItem = ({ cad }) => (
+	<li>
+		<p>{cad.companyCode}</p>
+		<p>{cad.companyNameUkr}</p>
+		<p>{cad.usreou}</p>
+		<p>{cad.index}</p>
+		<p>{cad.country}</p>
+		<p>{cad.region}</p>
+		<p>{cad.district}</p>
+		<p>{cad.city}</p>
+		<p>{cad.street}</p>
+		<p>{cad.houseNumber}</p>
+		<p>{cad.headPosition}</p>
+		<p>{cad.fullName}</p>
+		<p>{cad.bankName}</p>
+		<p>{cad.bankUsreou}</p>
+		<p>{cad.bankMfi}</p>
+		<p>{cad.bankSwift}</p>
+		<p>{cad.companyAccount}</p>
+		<p>{cad.accountCurrency}</p>
+		<p>{cad.currencyCode}</p>
+		<p>{cad.correspondentBankSwift}</p>
+		<p>{cad.correspondentBank}</p>
+		<p>{cad.bankAddress}</p>
+	</li>
+);
+
+export default GeoCadsItem;
