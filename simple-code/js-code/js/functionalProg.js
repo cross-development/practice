62dbfed8887569632f46ee066dea8f84d3ff7310
Vitaliy Derda <derdavitaliy@gmail.com>
2021-04-28 21:07:44 +0300

    added js practice

--- simple-code/js-code/js/functionalProg.js
+++ simple-code/js-code/js/functionalProg.js
@@ -9,3 +9,19 @@ const once = fn => {
 const unary = fn => {
 	fn.length === 1 ? fn : arg => fn(arg);
 };
+
+const curry = fn => {
+	if (typeof fn !== 'function') {
+		throw Error('No function provided');
+	}
+
+	return function curriedFn(...args) {
+		return fn.apply(null, args);
+	};
+};
+
+const multiply = (x, y, z) => x * y * z;
+
+curry(multiply)(1, 2, 3); // 6
+
+curry(multiply)(1, 2, 0); // 0
